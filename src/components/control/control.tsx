687539bdb12c0c0a60b3ef6777538ca2270cfeb4
Wen Tjun <wentjun289@hotmail.com>
2019-06-05 01:08:31 +0800

    added input for selection of number of taxis to be shown
    - input listens to same state as range slider, vice versa

--- src/components/control/control.tsx
+++ src/components/control/control.tsx
@@ -34,18 +34,32 @@ class Control extends React.Component<Props, State> {
   public render() {
     const wrapperStyle: CSSProperties = {
       width: '25vw',
-      height: '100vh'
+      height: '100vh',
+      display: 'flex',
+      flexFlow: 'column wrap'
+    };
+    const labelStyle: CSSProperties = {
+      textAlign: 'center'
     };
     return(
       <div style={wrapperStyle}>
+        <span style={labelStyle}> Number of Taxis to Display: </span>
         <input
           id="taxiRangeSlider"
           type="range"
-          min="0"
+          min="1"
           max="50"
           value={this.state.value}
           onChange={this.handleChange}
           step="1"
+        />
+        <input
+          id="taxi"
+          type="number"
+          min="1"
+          max="50"
+          value={this.state.value}
+          onChange={this.handleChange}
         />
       </div>
     );
