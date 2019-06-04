c4cfebf4dc24afc733e90ce3c847681bc1441ea2
Wen Tjun <wentjun289@hotmail.com>
2019-06-05 03:01:01 +0800

    debouncing of input values
    - create subscription for onChange event
    - used debounceTime to handle the debouncing of input values

--- src/components/control/control.tsx
+++ src/components/control/control.tsx
@@ -1,3 +1,5 @@
+import { from, Subject } from 'rxjs';
+import { debounceTime, mergeMap } from 'rxjs/operators';
 import React, { CSSProperties } from 'react';
 
 interface Props {
@@ -5,30 +7,58 @@ interface Props {
 }
 
 interface State {
-  value: any;
+  value: string;
 }
 
 class Control extends React.Component<Props, State> {
+  private rangeSliderSubject: Subject<string>;
 
   constructor(props: Props) {
     super(props);
     this.state = {
-      value: 5
+      value: '5'
     };
     // bind context of 'this' to event handler
-    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    // Subject to handle the debouncing of input values
+    this.rangeSliderSubject = new Subject<string>();
   }
 
   public componentDidMount() {
-
+    this.fetchTaxiList();
+    this.debounceInputSelection();
   }
 
   public componentWillUnmount() {
 
   }
 
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({value: Number(event.target.value)});
+  debounceInputSelection() {
+    this.rangeSliderSubject
+      .pipe(
+        debounceTime(1000)
+      ).subscribe((debouncedInputvalue: string) => {
+        this.fetchTaxiList();
+      });
+  }
+
+  handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const taxiAmount: string = event.target.value;
+    this.setState({value: taxiAmount});
+    this.rangeSliderSubject.next(taxiAmount);
+  }
+
+  fetchTaxiList() {
+    const fetchTaxiListRequest = fetch(`https://qa-interview-test.qa.splytech.io/api/drivers?latitude=51.5049375&longitude=-0.0964509&count=${this.state.value}`);
+    from(fetchTaxiListRequest)
+      .pipe(
+        mergeMap(response => response.json())
+      ).subscribe(res => {
+        console.log(res);
+      }, error => {
+        // handle network errors
+        console.log(error);
+      });
   }
 
   public render() {
@@ -50,7 +80,7 @@ class Control extends React.Component<Props, State> {
           min="1"
           max="50"
           value={this.state.value}
-          onChange={this.handleChange}
+          onChange={this.handleInputChange}
           step="1"
         />
         <input
@@ -59,7 +89,7 @@ class Control extends React.Component<Props, State> {
           min="1"
           max="50"
           value={this.state.value}
-          onChange={this.handleChange}
+          onChange={this.handleInputChange}
         />
       </div>
     );
